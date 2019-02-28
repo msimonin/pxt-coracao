1b4ca506ccbc872b11e3ee0a81a8a1e432c28dda
Parau Branco <parahuari@gmail.com>
2019-02-28 15:00:29 -0300

    Traduzindo nome das variaveis

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let ticks = 0
 let emptyObstacleY = 0
-let obstacles: game.LedSprite[] = []
+let obstaculo: game.LedSprite[] = []
 let index = 0
 let bird: game.LedSprite = null
 input.onButtonPressed(Button.A, () => {
@@ -10,25 +10,25 @@ input.onButtonPressed(Button.B, () => {
     bird.change(LedSpriteProperty.Y, 1)
 })
 index = 0
-obstacles = []
+obstaculo = []
 bird = game.createSprite(0, 2)
 bird.set(LedSpriteProperty.Blink, 300)
 basic.forever(() => {
-    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
-        obstacles.removeAt(0).delete()
+    while (obstaculo.length > 0 && obstaculo[0].get(LedSpriteProperty.X) == 0) {
+        obstaculo.removeAt(0).delete()
     }
-    for (let obstacle2 of obstacles) {
+    for (let obstacle2 of obstaculo) {
         obstacle2.change(LedSpriteProperty.X, -1)
     }
     if (ticks % 3 == 0) {
         emptyObstacleY = Math.randomRange(0, 4)
         for (let index2 = 0; index2 <= 4; index2++) {
             if (index2 != emptyObstacleY) {
-                obstacles.push(game.createSprite(4, index2))
+                obstaculo.push(game.createSprite(4, index2))
             }
         }
     }
-    for (let obstacle3 of obstacles) {
+    for (let obstacle3 of obstaculo) {
         if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
             game.gameOver()
         }
